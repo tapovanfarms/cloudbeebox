28e55c56e226e8bbf7967587c4c0f83e72aa6496
Mayank <mayankbhagya@gmail.com>
2021-03-16 17:32:49 +0530

    Trying to fetch card id

--- js/client.js
+++ js/client.js
@@ -8,8 +8,8 @@ TrelloPowerUp.initialize({
     "card-badges": function (t, opts) {
         let cardAttachments = opts.attachments; // Trello passes you the attachments on the card
         return t
-          .card("name")
-          .get("name")
+          .card("id")
+          .get("id")
           .then(function (cardName) {
             console.log("We just loaded the card name for fun: " + cardName);
             return [
